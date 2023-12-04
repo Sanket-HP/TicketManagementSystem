2c8a267efbfc8b43689aa0ecf080120b218b2fd4
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-12-04 10:30:48 +0530

    Update UserModel.py

--- backend/app/models/UserModel.py
+++ backend/app/models/UserModel.py
@@ -1,1 +1,24 @@
+# Import necessary modules and database connection
+
+class User:
+    def __init__(self, user_id, name, email, phone):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+    # Methods for accessing/modifying user attributes
+    def get_user_id(self):
+        return self.user_id
+
+    def get_name(self):
+        return self.name
+
+    def get_email(self):
+        return self.email
+
+    def get_phone(self):
+        return self.phone
+
+    # Other methods specific to user functionalities
 
