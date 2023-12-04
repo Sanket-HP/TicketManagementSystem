80c47f367f81b8e568cffaee88bbd3bfbb806d90
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-12-04 10:32:37 +0530

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,1 +1,19 @@
+from flask import Flask
+from flask_restful import Api
+from controllers.TicketController import TicketController
+from controllers.UserController import UserController
+
+app = Flask(__name__)
+api = Api(app)
+
+# Initialize controllers
+ticket_controller = TicketController()
+user_controller = UserController()
+
+# Define routes
+api.add_resource(ticket_controller, '/tickets', '/tickets/<int:ticket_id>')
+api.add_resource(user_controller, '/users', '/users/<int:user_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
