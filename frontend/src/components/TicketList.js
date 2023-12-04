4bfc7568538d92d3265d933542268a32a959522a
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-12-04 09:07:40 +0530

    Update TicketList.js

--- frontend/src/components/TicketList.js
+++ frontend/src/components/TicketList.js
@@ -1,1 +1,19 @@
+// TicketList.js
+import React from 'react';
+
+const TicketList = ({ tickets }) => {
+    return (
+        <section id="ticketList">
+            <h2>My Tickets</h2>
+            <ul>
+                {tickets.map((ticket, index) => (
+                    <li key={index}>{ticket.eventName} - {ticket.ticketType}</li>
+                    {/* Display other ticket details as needed */}
+                ))}
+            </ul>
+        </section>
+    );
+};
+
+export default TicketList;
 
