85d5f99635dcb228a4df3d67f44271729dd924ef
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-12-04 10:22:22 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,29 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AdminDashboard from './pages/AdminDashboard';
+import TicketForm from './components/TicketForm';
+import TicketList from './components/TicketList';
+
+const App = () => {
+    return (
+        <Router>
+            <div>
+                {/* Your header, navigation, or common components can go here */}
+                
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/admin" component={AdminDashboard} />
+                    <Route exact path="/create-ticket" component={TicketForm} />
+                    {/* Add more routes for different pages or components */}
+                </Switch>
+                
+                {/* Your footer or other common components */}
+            </div>
+        </Router>
+    );
+};
+
+export default App;
 
