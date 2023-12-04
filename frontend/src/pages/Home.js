691622e4dcf953a2188783795fa2efab9e10e308
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-12-04 10:17:14 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,1 +1,15 @@
+// HomePage.js
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to Ticket Management System</h1>
+            <p>Streamlining ticket generation and management</p>
+            {/* Add any additional content for the home page */}
+        </div>
+    );
+};
+
+export default HomePage;
 
