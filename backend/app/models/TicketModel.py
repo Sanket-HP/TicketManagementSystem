0d0e0ddd9e1bf0df93246d935a9fe77a5fe06c7a
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-12-04 10:30:30 +0530

    Update TicketModel.py

--- backend/app/models/TicketModel.py
+++ backend/app/models/TicketModel.py
@@ -1,1 +1,24 @@
+# Import necessary modules and database connection
+
+class Ticket:
+    def __init__(self, ticket_id, user_id, details, qr_code):
+        self.ticket_id = ticket_id
+        self.user_id = user_id
+        self.details = details
+        self.qr_code = qr_code
+
+    # Methods for accessing/modifying ticket attributes
+    def get_ticket_id(self):
+        return self.ticket_id
+
+    def get_user_id(self):
+        return self.user_id
+
+    def get_details(self):
+        return self.details
+
+    def get_qr_code(self):
+        return self.qr_code
+
+    # Other methods specific to ticket functionalities
 
