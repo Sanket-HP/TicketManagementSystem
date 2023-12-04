2250158723c97101f98f37265499b855a2ea7779
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-12-04 10:17:32 +0530

    Update AdminDashboard.js

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -1,1 +1,15 @@
+// AdminDashboard.js
+import React from 'react';
+
+const AdminDashboard = () => {
+    // Add logic for fetching admin-related data or functionality
+    return (
+        <div>
+            <h2>Admin Dashboard</h2>
+            {/* Display admin-specific functionalities or data */}
+        </div>
+    );
+};
+
+export default AdminDashboard;
 
