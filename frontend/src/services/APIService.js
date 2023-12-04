60a0d9e845afce5e10b5674916d16796b8f74db0
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-12-04 10:21:34 +0530

    Update APIService.js

--- frontend/src/services/APIService.js
+++ frontend/src/services/APIService.js
@@ -1,1 +1,28 @@
+// APIService.js
+const BASE_URL = 'https://your-backend-api-url.com'; // Replace with your actual backend API URL
+
+const APIService = {
+    createTicket: async (ticketData) => {
+        try {
+            const response = await fetch(`${BASE_URL}/tickets`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // Add any required headers (like authentication tokens) here
+                },
+                body: JSON.stringify(ticketData),
+            });
+            if (!response.ok) {
+                throw new Error('Error creating ticket');
+            }
+            return await response.json();
+        } catch (error) {
+            console.error('Error creating ticket:', error);
+            throw error;
+        }
+    },
+    // Add other API calls as needed (e.g., fetching ticket data, user information, etc.)
+};
+
+export default APIService;
 
