3c9d2f4a5703e738ab20469244e2b85d0ec70491
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-12-04 10:36:14 +0530

    Update 001_initial_migration.sql

--- database/migrations/001_initial_migration.sql
+++ database/migrations/001_initial_migration.sql
@@ -1,1 +1,9 @@
+-- Create users table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password_hash VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
